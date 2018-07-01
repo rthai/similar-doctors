aa84d1fda7a82c68df08e345ac93ea7524b5dffd
rthai <ahnamiko@gmail.com>
2018-07-01 01:19:22 -0700

    build search form

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { specialties, cities } from '../../db/data/generateDoctor.js';
 
 class App extends React.Component {
   constructor() {
@@ -8,7 +9,39 @@ class App extends React.Component {
 
     }
   }
-  render();
+
+  handleSpecialtySelection(e) {
+  }
+
+  handleCitySelection(e) {
+  }
+
+  handleFind() {
+    //check db for these doctors
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Find a Doctor</h1>
+        </header>
+        <form className="App-form">
+          <label htmlFor="search">Search</label>
+          <select name="specialty" onChange={this.handleSpecialtySelection}>
+            <option value="" defaultValue="disabled selected">Specialty</option>
+            {specialties.map( (specialty, index) => <option value={specialty} key={index}>{specialty}</option>)}
+          </select>
+          <label htmlFor="near">Near</label>
+          <select name="city" onChange={this.handleCitySelection}>
+            <option value="" defaultValue="disabled selected">City</option>
+            {cities.map( (city, index) => <option value={city} key={index}>{city}</option>)}
+          </select>
+          <button>Find</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
