6d10b831d880c2824818a872990ebe74c171323c
rthai <ahnamiko@gmail.com>
2018-07-01 08:54:08 -0700

    setup react component

---
+++ client/components/DoctorList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class DoctorList extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className="DoctorList">
+      </div>
+    );
+  }
+}
+
+export default DoctorList;
---
+++ client/components/DoctorListItem.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class DoctorListItem extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div className="DoctorListItem">
+      </div>
+    );
+  }
+}
+
+export default DoctorListItem;
