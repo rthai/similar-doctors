ea197887ade7a6db1dd9a797a89a6bfac984a586
rthai <ahnamiko@gmail.com>
2018-07-01 11:46:36 -0700

    add simple review stars

--- client/components/DoctorListItem.jsx
+++ client/components/DoctorListItem.jsx
@@ -9,17 +9,35 @@ class DoctorListItem extends React.Component {
   }
 
   expandList() {
-    this.setState({ showDetails: !this.state.showDetails })
+    this.setState({ showDetails: !this.state.showDetails });
   }
 
   render() {
-    const details = (     
+    const filledStars = Math.round(this.props.doctor.rating);
+
+    let stars = [
+      <span className="fa fa-star" key="0"></span>,
+      <span className="fa fa-star" key="1"></span>,
+      <span className="fa fa-star" key="2"></span>,
+      <span className="fa fa-star" key="3"></span>,
+      <span className="fa fa-star" key="4"></span>
+    ]
+
+    for (let i = 0; i < filledStars; i++) {
+      stars[i] = <span className="fa fa-star filled" key={i}></span>
+    }
+
+
+    let details = (     
       <div>
-        <img src={this.props.doctor.thumbnail}></img>
+        <span><img src={this.props.doctor.thumbnail}></img></span>
         <p>Address: {this.props.doctor.street_name}</p>
         <p>{`${this.props.doctor.city}, ${this.props.doctor.state}`}</p>
         <p>{this.props.doctor.phone_number}</p>
-        <p>{this.props.doctor.rating}</p>
+
+        {stars.map( star => star)}
+
+        <span>{` ${this.props.doctor.rating}`}</span>
       </div>
     ); 
 
