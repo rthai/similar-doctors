c834aaa81fde7c3951f015432fa59702816f6c6a
rthai <ahnamiko@gmail.com>
2018-07-01 14:04:23 -0700

    refactor component

--- client/components/DoctorList.jsx
+++ client/components/DoctorList.jsx
@@ -1,23 +1,10 @@
 import React from 'react';
 import DoctorListItem from './DoctorListItem.jsx';
 
-class DoctorList extends React.Component {
-  constructor(props) {
-    super(props)
-
-  }
-
-  render() {
-    return (
-      <ul className="DoctorList">
-        {this.props.doctors.length > 0 ? 
-        
-        this.props.doctors.map( dr => <DoctorListItem doctor={dr} key={dr._id}/>) : 
-        
-        'No doctors found.'}
-      </ul>
-    );
-  }
-}
+const DoctorList = (props) => (
+  <ul className="DoctorList">
+    {props.doctors.length > 0 ? props.doctors.map( dr => <DoctorListItem doctor={dr} key={dr._id}/>) : 'No doctors found.'}
+  </ul>
+);
 
 export default DoctorList;
