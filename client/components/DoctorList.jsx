dc33b19062596494ff9dbd5ad87328d05daab706
rthai <ahnamiko@gmail.com>
2018-07-01 10:41:08 -0700

    map doctorsList to doctorListItem

--- client/components/DoctorList.jsx
+++ client/components/DoctorList.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import DoctorListItem from './DoctorListItem.jsx';
 
 class DoctorList extends React.Component {
   constructor(props) {
     super(props)
+
   }
 
   render() {
     return (
-      <div className="DoctorList">
-      </div>
+      <ul className="DoctorList">
+        {this.props.doctors.length > 0 ? 
+        
+        this.props.doctors.map( dr => <DoctorListItem doctor={dr} key={dr._id}/>) : 
+        
+        'No doctors found.'}
+      </ul>
     );
   }
 }
