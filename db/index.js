5e913f5d4e45657e893f6c4a51a70c232bf4fa73
rthai <ahnamiko@gmail.com>
2018-07-01 17:05:45 -0700

    add docker mongoDB uri

--- db/index.js
+++ db/index.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
-const mongoUri = 'mongodb://localhost/doctors';
+// const mongoUri = 'mongodb://localhost/doctors';
+const mongoUriDocker = 'mongodb://database/doctors';
 
 // could have used BetterDoctor API for data instead of building a database and generating mock data
 
-const db = mongoose.connect(mongoUri);
+const db = mongoose.connect(mongoUriDocker);
 
 mongoose.connection.on('connected', () => {
   console.log('Mongoose connection open');
