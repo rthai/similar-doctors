0b11a8538e8cff799ac48a6c4b967de9b2f34413
rthai <ahnamiko@gmail.com>
2018-08-16 16:14:07 -0700

    change json -> csv data

--- db/data/generateDoctor.js
+++ db/data/generateDoctor.js
@@ -5,17 +5,11 @@ const specialties = `Cardiology,Orthopedics,Gastroenterology,Urology,Dermatology
 const cities = `San Francisco,Oakland,Palo Alto,Berkeley,San Jose,Mountain View,San Mateo,Redwood City,Menlo Park,Millbrae`.split(',').sort();
 
 const generateDoctor = () => {
-  const doctor = {
-    name: faker.fake('{{name.firstName}} {{name.lastName}}'),
-    thumbnail: faker.image.avatar(),
-    specialty: specialties[faker.random.number({min: 0, max: (specialties.length-1)})],
-    street_name: faker.address.streetAddress(),
-    city: cities[faker.random.number({min: 0, max: (cities.length-1)})],
-    state: 'CA',
-    phone_number: faker.phone.phoneNumberFormat(1),
-    rating: faker.finance.amount(1, 5, 1),
-  }
-  return JSON.stringify(doctor);
+  let doctor = '';
+
+  doctor += `${faker.fake('{{name.firstName}} {{name.lastName}}')},${faker.image.avatar()},${specialties[faker.random.number({min: 0, max: (specialties.length-1)})]},${faker.address.streetAddress()},${cities[faker.random.number({min: 0, max: (cities.length-1)})]},'CA',${faker.phone.phoneNumberFormat(1)},${faker.finance.amount(1, 5, 1)}\n`;
+  
+  return doctor;
 }
 
 module.exports = { 
--- db/data/writeDoctorList.js
+++ db/data/writeDoctorList.js
@@ -1,24 +1,23 @@
 const fs = require('fs');
 const { generateDoctor } = require('./generateDoctor.js');
 
-const stream = fs.createWriteStream('db/data/doctorList.json');
+const stream = fs.createWriteStream('db/data/doctorList.txt');
+const header = `name,thumbnail,specialty,street_name,city,state,phone_number,rating\n`;
 
-stream.write('[');
+stream.write(header);
 
 const write200x = () => {
-  let i = 200;
+  let i = 5;
   function write() {
     let ok = true;
     do {
       i -= 1;
       if (i === 0) {
         stream.write(generateDoctor(), 'utf-8', () => {
-          stream.write(']');
           stream.end();
         });
       } else {
         ok = stream.write(generateDoctor(), 'utf-8');
-        stream.write(',');
       }
     } while (i > 0 && ok);
     if (i > 0) {
