f1ab8f063eba06d1910c4928e310e961026c4e9b
rthai <ahnamiko@gmail.com>
2018-08-21 19:02:31 -0700

    rewrite tests to check json generated sample data

--- __tests__/generateDoctor.test.js
+++ __tests__/generateDoctor.test.js
@@ -1,15 +1,28 @@
 const { generateDoctor } = require("../db/data/generateDoctor");
 
-const doctor = generateDoctor();
+const doctor = JSON.parse(generateDoctor());
 
-describe('CSV Faker Doctor Data Generator', () => {
+describe('JSON Faker Doctor Data Generator', () => {
 
-  test('should generate doctor details as a string', () => {
-    expect(typeof doctor).toBe('string'); 
+  test('should generate sample data with appropriate value types', () => {
+    expect(typeof doctor).toBe('object');
+    expect(typeof doctor.name).toBe('string');
+    expect(typeof doctor.thumbnail).toBe('string'); 
+    expect(typeof doctor.specialty).toBe('string');
+    expect(typeof doctor.street_name).toBe('string');
+    expect(typeof doctor.city).toBe('string');
+    expect(typeof doctor.state).toBe('string'); 
+    expect(typeof doctor.phone_number).toBe('string'); 
+    expect(typeof parseInt(doctor.rating)).toBe('number'); 
   });
 
-  test('doctor details should have 8 fields', () => {
-    expect(doctor.split(',')).toHaveLength(8); 
+  test('sample doctor state should be CA', () => {
+    expect(doctor.state).toBe('CA'); 
+  });
+  
+  test('sample doctor rating should be an number between 1 and 5, inclusive', () => {
+    expect(parseInt(doctor.rating)).toBeGreaterThanOrEqual(1); 
+    expect(parseInt(doctor.rating)).toBeLessThanOrEqual(5); 
   });
 
 });
