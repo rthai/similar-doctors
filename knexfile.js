5123fa2c5083c6fec567873f9ed81e11965a9cea
rthai <ahnamiko@gmail.com>
2018-08-25 23:24:43 -0700

    add test and production settings

--- knexfile.js
+++ knexfile.js
@@ -4,5 +4,13 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: 'postgres://localhost/similar_doctors'
-  }
+  },
+  test: {
+    client: 'postgresql',
+    connection: 'postgres://localhost/similar_doctors'
+  },
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL + '?ssl=true'
+  },
 };
